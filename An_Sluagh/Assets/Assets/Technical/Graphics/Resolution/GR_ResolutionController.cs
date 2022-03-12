c94e66085eb11852209504f8b76b141de8c936de
Kailokk <Kailokk.converse@gmail.com>
2022-03-12 21:15:09 +0000

    Options Menu - Resolution

---
+++ An_Sluagh/Assets/Assets/Technical/Graphics/Resolution/GR_ResolutionController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GR_ResolutionController : MonoBehaviour
+{
+
+    [SerializeField]
+    private TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+    private bool isFullScreen = true;
+
+    int currentResolutionIndex;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("Fullscreen") == 0)
+        {
+            SetFullScreen(true);
+        }
+        else
+        {
+            SetFullScreen(false);
+        }
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        currentResolutionIndex = 0;
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = $"{resolutions[i].width} X {resolutions[i].height}";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+
+
+    public void SetFullScreen(bool fullScreen)
+    {
+        isFullScreen = fullScreen;
+        Screen.fullScreen = isFullScreen;
+        if (fullScreen == true)
+        {
+            PlayerPrefs.SetInt("Fullscreen", 0);
+            return;
+        }
+        PlayerPrefs.SetInt("Fullscreen", 1);
+    }
+
+
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, isFullScreen);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
